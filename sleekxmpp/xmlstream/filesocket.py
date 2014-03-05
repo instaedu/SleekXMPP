3c1007a3a320e5843c5f01cf80da16cdcb8756e0
Jay Farrimond <jay@instaedu.com>
2014-03-05 10:56:04 -0800

    correctly handle xmlns:client bug

--- sleekxmpp/xmlstream/filesocket.py
+++ sleekxmpp/xmlstream/filesocket.py
@@ -32,11 +32,12 @@ class FileSocket(_fileobject):
         data = self._sock.recv(size)
 
         # this makes me feel dirty... I hate iTeleportService
-        if data.find("xmlns:server"):
+        if data.find("xmlns:server") > -1:
             data = data.replace(
                 '<xmlns:server xmlns="http://www.w3.org/2000/xmlns/"/>', '')
             data = data.replace(
                 '<xmlns:server xmlns="http://www.w3.org/2000/xmlns/" />', '')
+        elif data.find("xmlns:client") > -1:
             data = data.replace(
                 '<xmlns:client xmlns="http://www.w3.org/2000/xmlns/"/>', '')
             data = data.replace(
